166b8b159fbd337b6363e099b6326035e22be92a
Meet Patel <meetaiims@yahoo.com>
2021-02-01 04:58:17 -0600

    add people service

--- modules/people/people.service.js
+++ modules/people/people.service.js
@@ -1,24 +1,27 @@
-const Queue = require('../queue/Queue')
-const store = require('../../store')
+const Queue = require('../queue/Queue');
+const store = require('../../store');
 
 // Set up initial data.
 // --------------------
 
-const people = new Queue()
-store.people.forEach(person => people.enqueue(person))
+const people = new Queue();
+store.people.forEach((person) => people.enqueue(person));
 
 // --------------------
 
 module.exports = {
   get() {
     // Return all people in the queue.
+    return people.all();
   },
 
-  enqueue(person) {
+  enqueue(name) {
     // Add a person to the queue.
+    return people.enqueue(name);
   },
 
-  dequeue() {
+  dequeue(person) {
     // Remove a person from the queue.
-  }
-}
+    return people.dequeue(person);
+  },
+};
