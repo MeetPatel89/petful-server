dc136806c4576e54c0ba39f7470737c8c04b65ab
Meet Patel <meetaiims@yahoo.com>
2021-01-31 21:54:17 -0600

    add GET / to get pets

--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -1,16 +1,16 @@
-const Queue = require('../queue/Queue')
-const store = require('../../store')
+const Queue = require('../queue/Queue');
+const store = require('../../store');
 
 // Set up initial data.
 // --------------------
 
 const pets = {
   cats: new Queue(),
-  dogs: new Queue()
-}
+  dogs: new Queue(),
+};
 
-store.cats.forEach(cat => pets.cats.enqueue(cat))
-store.dogs.forEach(dog => pets.dogs.enqueue(dog))
+store.cats.forEach((cat) => pets.cats.enqueue(cat));
+store.dogs.forEach((dog) => pets.dogs.enqueue(dog));
 
 // --------------------
 
