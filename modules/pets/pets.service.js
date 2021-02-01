3b91de19ba2b6aa9896b013d422dea9fa0c259ba
Meet Patel <meetaiims@yahoo.com>
2021-01-31 21:47:58 -0600

    add get method to pets service

--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -17,6 +17,11 @@ store.dogs.forEach(dog => pets.dogs.enqueue(dog))
 module.exports = {
   get() {
     // Return the pets next in line to be adopted.
+     let newPets = {
+       cat: pets.cats.show(),
+       dog: pets.dogs.show(),
+     };
+     return newPets;
   },
 
   dequeue(type) {
