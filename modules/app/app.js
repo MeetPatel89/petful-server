f5fb3cbf3d270eaa9e355d6b159115bd87c2017a
Meet Patel <meetaiims@yahoo.com>
2021-02-01 05:11:31 -0600

    refactor after postman

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use('/people', require('../people/people.router'))
-app.use('/pets', require('../pets/pets.router'))
+app.use('/people', require('../people/people.router'));
 
-module.exports = app
+app.use('/dog-queue', require('../DogQueue/dogqueue.router'));
+app.use('/cat-queue', require('../CatQueue/catqueue.router'));
+
+app.use('/pets', require('../pets/pets.router'));
+
+app.use('/cats', require('../pets/Cats/cats.router'));
+app.use('/dogs', require('../pets/Dogs/dogs.router'));
+
+module.exports = app;
